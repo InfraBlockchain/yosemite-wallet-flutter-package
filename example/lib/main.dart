aae65c06f90900535992c17d95ef32edd37ed715
Joe Park <readytopark@gmail.com>
2019-06-05 00:01:58 -0700

    Configuring ide

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -4,6 +4,7 @@ import 'dart:typed_data';
 
 import 'package:flutter/material.dart';
 import 'package:yosemite_wallet/yosemite_chain.dart';
+import 'package:yosemite_wallet/yosemite_wallet.dart';
 
 void main() => runApp(MyApp());
 
